fb8e1b52aefc7ad30f18cb89b95b7e70503eea7d
HAreemA <ahareem134@gmail.com>
2024-09-25 15:43:42 +0500

    First commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+export { default as RenderApp } from "../src/App.jsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
