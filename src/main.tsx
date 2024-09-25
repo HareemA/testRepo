d99bafb4fc7a9357615ebf1600de9389e9dc051f
HAreemA <ahareem134@gmail.com>
2024-09-25 16:38:26 +0500

    First commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@ export { default as RenderApp } from "../src/App.jsx";
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from "./Components/Home.tsx";
 import './index.css';
+import About from "./Components/About.tsx";
 
 // Define the router with routes
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home />, // Main app component that handles internal routing
+    element: <About />, // Main app component that handles internal routing
   },
   
   
